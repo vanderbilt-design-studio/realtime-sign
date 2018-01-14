157ff140e5038385df37b7b0f0a32fa6d5fed83e
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2018-01-14 16:41:29 -0600

    Minimize diffs

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const micro = require('micro');
 const WebSocket = require('ws');
 const { json, createError } = micro;
+const equal = require('fast-deep-equal');
 
 let wss;
-let lastStatus = {};
+let currStatus = {};
 
-const sendStatus = sock => sock.send(JSON.stringify(lastStatus));
+const sendStatus = sock => sock.send(JSON.stringify(currStatus));
 
 const server = micro(async (req, res) => {
     switch (req.method) {
@@ -13,14 +14,18 @@ const server = micro(async (req, res) => {
             if (req.headers['x-api-key'] !== process.env.RSIGN_API_KEY) {
                 throw createError(401, 'Invalid API key');
             }
-            lastStatus = await json(req);
+            const newStatus = await json(req);
+            if (equal(currStatus, newStatus)) {
+                return { updated: false };
+            }
+            currStatus = newStatus;
             wss.clients.forEach(sock => {
                 if (sock.readyState === WebSocket.OPEN) {
                     sendStatus(sock);
                 }
             });
-            console.log(lastStatus);
-            return { result: 'OK' };
+            console.log(currStatus);
+            return { updated: true };
         default:
             throw createError(405, 'Invalid method');
     }
